0f56aaaac185b8c7221d0deb6d4369ff9f49eeee
aivan <aivan.cva@gmail.com>
2024-11-12 00:27:35 -0600

    Add final styles

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const buttons = document.querySelectorAll(".btn");
+
+let operation = "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handle_input(e.target.value);
+  });
+});
+
+const handle_input = (input) => {
+  operation += input;
+  console.log(operation);
+};
