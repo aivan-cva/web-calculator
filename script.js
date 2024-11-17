17f3cc59076ea74d8ad5718b24f73e323c25727b
aivan <aivan.cva@gmail.com>
2024-11-17 00:14:21 -0600

    Add screen functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const buttons = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
 
-let operation = { firtsNum: "0", operand: "", lastNum: "0" };
+let operation = {
+  firtsNum: "0",
+  operand: "",
+  lastNum: "",
+};
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -20,19 +25,23 @@ const handle_input = (input) => {
       if (!operation.operand) {
         if (operation.firtsNum == "0") {
           operation.firtsNum = input.value;
+          updateScreen(operation);
           return operation;
         }
         if (operation.firtsNum != "0") {
           operation.firtsNum += input.value;
+          updateScreen(operation);
           return operation;
         }
       } else if (operation.operand != "") {
         if (operation.lastNum == "0") {
           operation.lastNum = input.value;
+          updateScreen(operation);
           return operation;
         }
         if (operation.lastNum != "0") {
           operation.lastNum += input.value;
+          updateScreen(operation);
           return operation;
         }
       }
@@ -41,6 +50,7 @@ const handle_input = (input) => {
     case "operand":
       if (operation.operand == "") {
         operation.operand = input.value;
+        updateScreen(operation);
         return operation;
       }
 
@@ -49,25 +59,42 @@ const handle_input = (input) => {
         clearOperation();
         operation.firtsNum = String(res);
         operation.operand = input.value;
+        updateScreen(operation);
         return operation;
       }
 
+      updateScreen(operation);
       return operation;
 
     case "dot":
       if (operation.operand == "" && !operation.firtsNum.includes(".")) {
         operation.firtsNum += ".";
+        updateScreen(operation);
         return operation;
       } else if (operation.operand != "" && !operation.lastNum.includes(".")) {
         operation.lastNum += ".";
+        updateScreen(operation);
         return operation;
       }
+      updateScreen(operation);
       return operation;
 
+    case "clear":
+      if (input.value == "AC") {
+        clearOperation();
+        updateScreen(operation);
+        return operation;
+      }
+
     case "equal":
+      if (operation.firtsNum && !operation.operand && !operation.lastNum) {
+        updateScreen(operation);
+        return operation;
+      }
       let res = resolveOperation(operation);
       clearOperation();
       operation.firtsNum = String(res);
+      updateScreen(operation);
       return operation;
 
     default:
@@ -85,7 +112,6 @@ const resolveOperation = (operation) => {
   switch (operation.operand) {
     case "addition":
       return firts + last;
-      break;
     case "subtraction":
       return firts - last;
     case "division":
@@ -101,4 +127,28 @@ const clearOperation = () => {
   operation.firtsNum = "0";
   operation.operand = "";
   operation.lastNum = "0";
+};
+
+const updateScreen = ({ firtsNum, operand, lastNum }) => {
+  if (lastNum == "0") {
+    lastNum = "";
+  }
+  let symbol = "";
+  switch (operand) {
+    case "addition":
+      symbol = "+";
+      break;
+    case "subtraction":
+      symbol = "-";
+      break;
+    case "multiplication":
+      symbol = "*";
+      break;
+    case "division":
+      symbol = "/";
+      break;
+    default:
+      break;
+  }
+  screen.textContent = firtsNum + symbol + lastNum;
 };
